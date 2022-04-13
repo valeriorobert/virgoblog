c54d6d28c178d68fbbd691dc7c3ac3112e2c8072
valeriorobert <valerio290194@hotmail.it>
2022-04-13 19:12:24 +0200

    bho

--- cursore.js
+++ cursore.js
@@ -1,27 +1,4 @@
-const sfera = document.getElementById("sfera")
 
 
-let currentX = 0
-let currentY = 0
-let aimX = 0
-let aimY = 0
-let speed = 0.2
 
 
-const animate = function () {
-    currentX += (aimX + currentX) * speed
-    currentY += (aimY + currentY) * speed
-
-
-    sfera.style.left = currentX + "px"
-    sfera.style.top = currentY + "px"
-    requestAnimationFrame(animate)
-}
-
-animate()
-const onMouseMove = (event) => {
-    aimX = event.pageX + "px"
-    aimY = event.pageY + "px"
-
-}
-document.addEventListener("mousemove", onMouseMove)
